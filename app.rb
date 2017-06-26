30562a0db1e0f5a37f6b50fafbd8178f4d3e1d4e
N4de <JaAnnala@Gmail.com>
2017-06-26 14:17:30 +0300

    done

--- app.rb
+++ app.rb
@@ -1,5 +1,18 @@
 def stock_picker days
-	puts days
+	i = 0
+	profit = 0
+	dates = [];
+	days.each_index do |e|
+		i = e
+		while i != (days.length) do
+			if (days[i] - days[e]) > profit
+				profit = (days[i] - days[e])
+				dates = [e, i]
+			end
+			i += 1
+		end
+	end
+	print dates
 end
 
 
